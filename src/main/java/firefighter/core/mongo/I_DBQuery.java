3e056c22dcf7ea64490139319117fafce20bd9cf
romanow-nsk <romanow@ngs.ru>
2020-12-03 13:10:48 +0700

    11

--- src/main/java/firefighter/core/mongo/I_DBQuery.java
+++ src/main/java/firefighter/core/mongo/I_DBQuery.java
@@ -9,11 +9,12 @@ public interface I_DBQuery {
     public final static int ModeLT=1;
     public final static int ModeLTE=2;
     public final static int ModeEQ=3;
-    public final static int ModeNEQ=3;
+    public final static int ModeNEQ=4;
+    public final static int ModeGT=5;
     public final static String sqlModeList[]={"AND","OR"};
     public final static String modeList[]={"$and","$or"};
-    public final static String sqlCmpList[]={">=","<","<","=","!="};
-    public final static String cmpList[]={"$gte","$lt","$lte","$eq","$ne"};
+    public final static String sqlCmpList[]={">=","<","<","=","!=",">"};
+    public final static String cmpList[]={"$gte","$lt","$lte","$eq","$ne","$gt"};
     public BasicDBObject getQuery();
     public String getWhere();
     }
